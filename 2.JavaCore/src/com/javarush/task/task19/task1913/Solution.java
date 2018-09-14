fc333a6c2d57318ec60816fd9e34d3ef3e7f9199
sm.yusupov@mail.ru <sm.yusupov@mail.ru>
2018-09-14 17:31:44 +0300

    2

--- 2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
@@ -4,10 +4,21 @@ package com.javarush.task.task19.task1913;
 Выводим только цифры
 */
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream byteAOS = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteAOS);
+        System.setOut(printStream);
+        testString.printSomething();
+        String s = byteAOS.toString().replaceAll("\\D",  "");
+        System.setOut(consoleStream);
+        System.out.println(s);
     }
 
     public static class TestString {
