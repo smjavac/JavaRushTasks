450067dc6978601ff13333ba3a0c6c3a4ac69308
admin <sm.yusupov@mail.ru>
2018-10-02 13:44:35 +0300

    first commit

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -14,6 +14,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -32,47 +34,58 @@ import java.util.regex.Pattern;
 //            }
 //            text = stringBuilder.toString();
 //            String tag = args[0];
+//            String[] array;
+//            List<Integer> list1 = new ArrayList<>();
+//            List<Integer> list2 = new ArrayList<>();
+
 //            Document document = Jsoup.parse(text,  "", Parser.xmlParser());
 //            Elements elements = document.select(tag);
 //            for (Element s: elements) System.out.println(s);
-//
-//
+
+
 //        }
 //    }
 //}
-
-public class Solution{
-    public static void main(String[] args) throws IOException{
-        args=new String[]{"span"};
-        String tag=args[0];
-        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
-        String file="f1";//reader.readLine();
-        BufferedReader fileReader=new BufferedReader(new FileReader(file));
-        reader.close();
-        StringBuilder sb=new StringBuilder();
-        while(fileReader.ready()) sb.append(fileReader.readLine());
-        fileReader.close();
-        String s=sb.toString().replaceAll("^.*?<"+tag,"<"+tag);
-        s=s.replaceAll("<"+tag,"►").replaceAll("</"+tag+">","◄");
-        System.out.println(s+"\n\n{");
-        Matcher m;
-        sb.setLength(0);
-        sb.append("►[^►]*?◄");
-        boolean find=false;
-        int i=1;
-        while(true){
-            m=Pattern.compile(sb.toString()).matcher(s);
-            while(m.find()){
-                if(i>2) System.out.println("\nregex:="+sb);
-                System.out.println(file=i+"|"+m.group());//.replaceAll("►","<"+tag).replaceAll("◄","</"+tag+">"));
-                find=true;
-            }
-            if(find){
-                sb.insert(0,"►[^►]*?");
-                sb.append("[^►]*?◄");
-                find=false;
-                i++;
-            }else break;
+public class Solution {
+    public static void main(String[] args)throws IOException {
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        String path  = consoleReader.readLine();
+        consoleReader.close();
+        BufferedReader fileReader = new BufferedReader(new FileReader(path));
+        StringBuilder stringBuilder = new StringBuilder();
+        while (fileReader.ready()){
+            stringBuilder.append(fileReader.readLine());
         }
+        fileReader.close();
+        String data = stringBuilder.toString();
+
+        String startTag = "<" + args[0];
+        String endTag = "</" + args[0] + ">";
+        int startTagIndex;
+        int endTagIndex;
+        int tempStartTagIndex = 0;
+        while(tempStartTagIndex != -1){
+            startTagIndex = data.indexOf(startTag, tempStartTagIndex);
+            System.out.println("startTagIndex: " + startTagIndex);
+            if(startTagIndex == -1 ){
+                return;
+            }
+            endTagIndex = data.indexOf(endTag, startTagIndex);
+            System.out.println("endTagIndex: " + endTagIndex);
+            tempStartTagIndex = data.indexOf(startTag, startTagIndex + 1);
+            System.out.println("tempStartTagIndex :" + tempStartTagIndex);
+
+            if(tempStartTagIndex < endTagIndex && tempStartTagIndex != -1){
+                endTagIndex = data.indexOf(endTag, endTagIndex + 1);
+                System.out.println("endTagIndex: " + endTagIndex);
+            }
+
+            System.out.println(data.substring(startTagIndex, endTagIndex + endTag.length()));
+
+
+        }
+
+
     }
 }
+
