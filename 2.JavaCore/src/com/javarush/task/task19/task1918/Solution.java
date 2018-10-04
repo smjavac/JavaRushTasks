a9326d436790e335b3388e338b73c54c444127aa
sm.yusupov@mail.ru <sm.yusupov@mail.ru>
2018-10-04 11:39:02 +0300

    1

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -4,7 +4,24 @@ package com.javarush.task.task19.task1918;
 Знакомство с тегами
 */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(br.readLine()))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String s;
+            while (bufferedReader.ready()){
+                stringBuilder.append(bufferedReader.readLine());
+            }
+
+            s = stringBuilder.toString();
+
+            
+        }
     }
 }
