71c7d5ebd19c30021dd76c5bce877d69fae018f2
admin <sm.yusupov@mail.ru>
2018-10-15 12:43:57 +0300

    first commit

--- 2.JavaCore/src/com/javarush/task/task20/task2007/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2007/Solution.java
@@ -1,7 +1,8 @@
 package com.javarush.task.task20.task2007;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /* 
@@ -12,7 +13,14 @@ public class Solution {
         public List<User> users = new ArrayList<>();
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        // код ниже для проверки, нужна ли сериализация класса Uzer
+//        JavaRush javaRush = new JavaRush();
+//        User user = new User();
+//        user.setBirthDate(new Date());
+//        javaRush.users.add(user);
+//        FileOutputStream fileOutputStream = new FileOutputStream("D:\\java\\IDEA\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task20\\task2007\\file");
+//        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+//        for (User u: javaRush.users) objectOutputStream.writeObject(u);
     }
 }
