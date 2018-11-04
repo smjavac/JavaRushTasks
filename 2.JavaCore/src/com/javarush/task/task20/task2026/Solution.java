c03dd1c991b6a8d16d9f853552bb207347d83e05
admin <sm.yusupov@mail.ru>
2018-11-04 14:11:57 +0300

    first commit

--- 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task20.task2026;
 
-/* 
+import java.util.Arrays;
+
+/*
 Алгоритмы-прямоугольники
 */
 public class Solution {
@@ -22,6 +24,15 @@ public class Solution {
         System.out.println("count = " + count1 + ". Должно быть 2");
         int count2 = getRectangleCount(a2);
         System.out.println("count = " + count2 + ". Должно быть 4");
+        for (int i = 0; i < a1.length; i++){
+            byte[] a = a1[i];
+            for (int j = 0; j < a.length; j++) System.out.print(a[j]);
+            System.out.println();
+        }
+        System.out.println(Arrays.toString(a1));
+
+
+
     }
 
     public static int getRectangleCount(byte[][] a) {
