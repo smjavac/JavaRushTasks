1e806a580b8f12ba0a9e674d243086375096c29e
admin <sm.yusupov@mail.ru>
2018-11-05 17:52:04 +0300

    first commit

--- 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task20.task2027;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /* 
@@ -23,8 +24,42 @@ same - (1, 1) - (4, 1)
     }
 
     public static List<Word> detectAllWords(int[][] crossword, String... words) {
+           // String [] strings = words.clone();
+        List<Word> list = new ArrayList<>();
+        String text;
+        int startX;
+        int startY;
+        int endX;
+        int endY;
+            char [][] chars = new char[crossword.length + 1][crossword[0].length+1];
+             for (int i = 0; i < crossword.length; i++){
+                for (int j = 0; j < crossword[i].length; j++){
+                    chars[i][j] = (char) crossword[i][j];
 
-        return null;
+                }
+             }
+
+             for (String s: words){
+                 text = s;                                             //'f', 'd', 'e', 'r', 'l', 'k'
+              //   char [] ch = text.toCharArray();                    // 'u', 's', 'a', 'm', 'e', 'o'
+             //    for (int i = 0; i < ch.length; i++) {               // 'l', 'n', 'g', 'r', 'o', 'v'
+                    for (int i = 0; i < chars.length; i++){            // 'm', 'l', 'p', 'r', 'r', 'h'
+                        for (int j = 0; j < chars[i].length; j++){     // 'p', 'o', 'e', 'e', 'j', 'j'
+                            if (crossword[i][j] == text.indexOf(crossword[i][j],0)){
+                                try {
+                                    if (crossword[i-1][j-1] == text.indexOf(crossword[i][j],1)){
+
+                                    }
+                                }catch (ArrayIndexOutOfBoundsException e){
+
+                                }
+                            }
+                        }
+                    }
+          //     }
+             }
+      //  list.add(new Word(text));
+        return list;
     }
 
     public static class Word {
