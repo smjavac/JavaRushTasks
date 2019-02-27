54f61727d3fec045bd99f25beaadce1eb0818f1b
smjavac <sm.yusupov@mail.ru>
2019-02-27 20:01:23 +0300

    work comp

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2112/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2112/Solution.java
@@ -9,7 +9,7 @@ public class Solution {
             fakeConnection.unsupportedOperation();
 
         } catch (Exception e) {
-            
+
         }
     }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task21.task2113;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hippodrome {
+
+    private List<Horse>  horses;
+
+    static Hippodrome game;
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    public static void main(String[] args) {
+      //  game = new Hippodrome()
+        Horse horse1 = new Horse("horse1", -3, 0);
+        Horse horse2 = new Horse("horse2", -3, 0);
+        Horse horse3 = new Horse("horse3", -3, 0);
+
+    }
+}
